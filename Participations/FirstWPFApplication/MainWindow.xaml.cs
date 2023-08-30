04d8d68c448a0e7aeca1c25e466eb3265393d1c9
Ackerman, Adam P <adam@ou.edu>
2023-08-30 15:27:02 -0500

    Completed first participation

--- Participations/FirstWPFApplication/MainWindow.xaml.cs
+++ Participations/FirstWPFApplication/MainWindow.xaml.cs
@@ -20,9 +20,49 @@ namespace FirstWPFApplication
     /// </summary>
     public partial class MainWindow : Window
     {
+
+        List<Student> students = new List<Student>();
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void btnCreateStudent_Click(object sender, RoutedEventArgs e)
+        {
+            string name = txtName.Text;
+            DateTime? birthdate = Convert.ToDateTime(txtDob.Text);
+
+            Student student = new Student(); 
+            student.Birthdate = birthdate;
+            student.Name = name;
+
+            students.Add(student);
+            ClearTextBoxes();
+
+            MessageBox.Show($"{student.Name} was created and they are {student.CalculateAge().ToString("N0")} years old.");
+        }
+
+        private void btnCreateStudent_MouseEnter(object sender, MouseEventArgs e)
+        {
+            this.Background = Brushes.Green;
+        }
+
+        private void btnCreateStudent_MouseLeave(object sender, MouseEventArgs e)
+        {
+            this.Background = Brushes.Red;
+        }
+
+        private void btnClear_Click(object sender, RoutedEventArgs e)
+        {
+            ClearTextBoxes();
+        }
+
+        private void ClearTextBoxes()
+        {
+            //clear the textboxes
+            txtName.Text = string.Empty;
+            txtDob.Text = string.Empty;
+            txtName.Focus();
         }
     }
 }
---
+++ Participations/FirstWPFApplication/Student.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstWPFApplication
+{
+    public class Student
+    {
+
+        public string Name { get; set; }
+        public DateTime? Birthdate { get; set; }
+
+
+        /// <summary>
+        /// Default constructor
+        /// </summary>
+        public Student()
+        {
+            Name = string.Empty;
+            Birthdate = null;
+        }
+
+        /// <summary>
+        /// Calculates the age of the student
+        /// </summary>
+        /// <returns>The number of years old the student is as a whole number</returns>
+        public int CalculateAge()
+        {
+            if (Birthdate.HasValue)
+            {
+                DateTime now = DateTime.Today;
+                int age = now.Year - Birthdate.Value.Year;
+                if (Birthdate.Value > now.AddYears(-age))
+                {
+                    age--;
+                }
+                return age;
+            }
+            else
+            {
+                return -1;
+            }
+        }
+
+
+
+    }
+
+
+}
