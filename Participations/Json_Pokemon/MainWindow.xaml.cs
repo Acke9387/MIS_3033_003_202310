e24c6f8fbb5dc91a5344055d026fd3dd9a6cebf4
Ackerman, Adam P <adam@ou.edu>
2023-09-20 15:42:22 -0500

    started the pokemon participation

---
+++ Participations/Json_Pokemon/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Json_Pokemon
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            string url = @"https://pokeapi.co/api/v2/pokemon?offset=00&limit=1300";
+
+            using (var client = new HttpClient())
+            {
+                string pokemonJson = client.GetStringAsync(url).Result;
+
+                PokemonApi api = JsonConvert.DeserializeObject<PokemonApi>(pokemonJson);
+
+                api.results.OrderBy(pokemon => pokemon.name).ToList().ForEach(pokemon =>
+                {
+                    cboPokemen.Items.Add(pokemon);
+                });
+            }
+
+            
+
+        }
+    }
+}
---
+++ Participations/Json_Pokemon/PokemonApi.cs
@@ -0,0 +1,25 @@
+﻿
+using System.Collections.Generic;
+
+namespace Json_Pokemon
+{
+    public class PokemonApi
+    {
+
+        public List<PokemonResult> results { get; set; }
+
+    }
+
+    public class PokemonResult
+    {
+        public string name { get; set; }
+
+        public string url { get; set; }
+
+        public override string ToString()
+        {
+            return name;
+        }
+
+    }
+}
