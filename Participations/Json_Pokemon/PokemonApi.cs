840122ef67d8d1994f6c3d6ee3ed3be008db23a3
Ackerman, Adam P <adam@ou.edu>
2023-09-21 08:52:13 -0500

    started pokemon

--- Participations/Json_Pokemon/PokemonApi.cs
+++ Participations/Json_Pokemon/PokemonApi.cs
@@ -1,17 +1,18 @@
 ﻿
+using Newtonsoft.Json;
 using System.Collections.Generic;
 
 namespace Json_Pokemon
 {
     public class PokemonApi
     {
-
         public List<PokemonResult> results { get; set; }
 
     }
 
     public class PokemonResult
     {
+        [JsonProperty("name")]
         public string name { get; set; }
 
         public string url { get; set; }
