0a9208505cbc738d50b55b6a2e1ff217771ff5df
Ackerman, Adam P <adam@ou.edu>
2023-09-25 15:45:31 -0500

    Did an in class example over connecting to the database

---
+++ In_Class_Examples/EntityFramework_ReadingFromDatabase/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using EntityFramework_ReadingFromDatabase.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace EntityFramework_ReadingFromDatabase
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private DB_128040_practiceContext db = new DB_128040_practiceContext();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCollectData_Click(object sender, RoutedEventArgs e)
+        {
+            foreach (FootballSchedule item in db.FootballSchedules)
+            {
+                if (item.IsHomeGame == true)
+                {
+                    lstHome.Items.Add(item); 
+                }
+                else
+                {
+                    lstAway.Items.Add(item);
+                }
+            }
+        }
+    }
+}
