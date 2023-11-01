c2621b65cc7650b0d18508bd8b60017217eea092
Ackerman, Adam P <adam@ou.edu>
2023-11-01 15:50:35 -0500

    Did our first example over ASP.NET MVC

---
+++ In_Class_Examples/WebVersionOfChuckNorris/Controllers/ChuckNorrisController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using WebVersionOfChuckNorris.Models;
+
+namespace WebVersionOfChuckNorris.Controllers
+{
+    public class ChuckNorrisController : Controller
+    {
+        public IActionResult Index()
+        {
+
+            string url = "https://api.chucknorris.io/jokes/random";
+            ChuckNorrisAPI chuckNorrisAPI;
+            using (var client = new HttpClient())
+            {
+                var json = client.GetStringAsync(url).Result;
+
+                 chuckNorrisAPI = JsonConvert.DeserializeObject<ChuckNorrisAPI>(json);
+
+            }
+            chuckNorrisAPI.icon_url = "https://4.bp.blogspot.com/-AqhMrSKWgCw/WNgmZTvmsVI/AAAAAAAAaTg/Q_FJ21XOArUw2fOMrsCGfARlD9336BmNwCLcB/s1600/20%2BGifs%2BChuck%2BNorris%2B7.gif";
+            return View(chuckNorrisAPI);
+        }
+    }
+}
---
+++ In_Class_Examples/WebVersionOfChuckNorris/Models/ChuckNorrisAPI.cs
@@ -0,0 +1,12 @@
+﻿namespace WebVersionOfChuckNorris.Models
+{
+    public class ChuckNorrisAPI
+    {
+        public string url { get; set; }
+
+        public string icon_url { get; set; }
+
+        public string value { get; set; }
+
+    }
+}
